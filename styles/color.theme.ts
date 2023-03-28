dc0dd8e54b2ff6c109aa2a9c17355d8fb8484ca3
Ubinquitous <ubinquitous1@gmail.com>
2023-03-28 09:53:32 +0900

    DELETE: ignore yarn.lock, add some library

---
+++ styles/color.theme.ts
@@ -0,0 +1,19 @@
+const color = {
+	primary: '#3772F7',
+	primary_container: '#F6FBFF',
+	on_primary_container: '#5F8EF9',
+	secondary: '#1C3E8A',
+	secondary_container: '#ECF3FF',
+	on_secondary_container: '#4964A1',
+	tertiary: '#303441',
+	tertiary_container: '#F6F6F6',
+	on_tertiary: '#DDDDDD',
+	on_tertiary_container: '#B3B3B3',
+	black: '#121212',
+	gray: '#898989',
+	white: '#FFFFFF',
+	plan: '#19D792',
+	line: '#D9D9D9',
+}
+
+export default color
---
+++ styles/text.theme.ts
@@ -0,0 +1,64 @@
+const textStyle = {
+	eng_12_bk: {
+		fontSize: '12px',
+		fontWeight: 900,
+	},
+	H1_26_EB: {
+		fontSize: '26px',
+		fontWeight: 800,
+	},
+	H2_22_B: {
+		fontSize: '22px',
+		fontWeight: 700,
+	},
+	H3_20_B: {
+		fontSize: '20px',
+		fontWeight: 700,
+	},
+	H3_20_R: {
+		fontSize: '20px',
+		fontWeight: 400,
+	},
+	H4_18_B: {
+		fontSize: '18px',
+		fontWeight: 700,
+	},
+	H4_18_R: {
+		fontSize: '18px',
+		fontWeight: 400,
+	},
+	Body1_16_B: {
+		fontSize: '16px',
+		fontWeight: 600,
+	},
+	Body1_16_M: {
+		fontSize: '16px',
+		fontWeight: 500,
+	},
+	Body2_14_B: {
+		fontSize: '14px',
+		fontWeight: 600,
+	},
+	Body2_14_M: {
+		fontSize: '14px',
+		fontWeight: 500,
+	},
+	Body3_12_B: {
+		fontSize: '12px',
+		fontWeight: 600,
+	},
+	Body3_12_M: {
+		fontSize: '12px',
+		fontWeight: 500,
+	},
+	Caption_12_R: {
+		fontSize: '12px',
+		fontWeight: 400,
+	},
+	Overline_10_R: {
+		fontSize: '10px',
+		fontWeight: 400,
+	},
+}
+
+export default textStyle
