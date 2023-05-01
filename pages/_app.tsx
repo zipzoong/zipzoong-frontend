3de80bb9906d37845aa2b61f124342496fb95580
Ubinquitous <ubinquitous1@gmail.com>
2023-05-01 22:38:56 +0900

    ADD : connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import Header from '@/components/Header'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default async function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Header />
