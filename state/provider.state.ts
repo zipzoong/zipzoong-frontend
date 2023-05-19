1cc7b4fbc83fbc35f2b98e2ee7c34deffaaffe57
Ubinquitous <ubinquitous1@gmail.com>
2023-05-19 21:02:43 +0900

    SET : provider init state

---
+++ state/provider.state.ts
@@ -0,0 +1,39 @@
+const providerState = [
+	{
+		phone: '',
+		profile_image_url: '',
+		introduction: {
+			images: [
+				{
+					id: '',
+					url: '',
+				},
+			],
+			title: '',
+			content: '',
+		},
+		expertise: {
+			super_category_id: '',
+			super_category_name: '',
+			sub_expertises: [
+				{
+					sub_category_id: '',
+					sub_category_name: '',
+				},
+			],
+		},
+		address: {
+			first: '',
+			second: '',
+		},
+		business_registration_num: '',
+		type: '',
+		id: '',
+		name: '',
+		email: '',
+		created_at: '',
+		updated_at: '',
+	},
+]
+
+export default providerState
