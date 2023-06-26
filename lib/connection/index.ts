02c13e46d284e604f65ad899bb575d8156c78e9a
Ubinquitous <ubinquitous1@gmail.com>
2023-06-26 15:53:50 +0900

    UPDATE : 의존성 업데이트, 스웨거 로컬 설정

--- lib/connection/index.ts
+++ lib/connection/index.ts
@@ -1,9 +1,10 @@
-import { IConnection } from '@zipzoong/sdk'
-import { Storage } from '@/lib/storage'
+import { IConnection } from '@zipzoong/sdk';
+import { Storage } from '@/lib/storage';
 
 const connection: IConnection = {
-	host: 'localhost:3000/api',
-	headers: { Authorization: `Bearer ${Storage.getItem('access_token')}` },
-}
+  host: 'http://localhost:6060',
+  simulate: true,
+  headers: { Authorization: `Bearer ${Storage.getItem('access_token')}` }
+};
 
-export default connection
+export default connection;
