915da13c963058a5dc4fbb1a2d54356f59e2828e
Ubinquitous <ubinquitous1@gmail.com>
2023-06-26 15:52:36 +0900

    ADD : MyPage Layout

--- layouts/MyPageLayout.style.ts
+++ layouts/MyPageLayout.style.ts
@@ -1,82 +1,160 @@
-import color from '@/styles/color.theme'
-import textStyle from '@/styles/text.theme'
-import Image from 'next/image'
-import styled from 'styled-components'
+import color from '@/styles/color.theme';
+import textStyle from '@/styles/text.theme';
+import Image from 'next/image';
+import styled, { css } from 'styled-components';
 
 export const MyPageLayout = styled.div`
-	margin-top: 40px;
-	width: 100vw;
-	min-height: 100vh;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-`
+  margin-top: 40px;
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export const MyPageContainer = styled.div`
-	width: 74vw;
-	height: 100vh;
-	display: flex;
-	gap: 24px;
-`
+  width: 74vw;
+  height: 100vh;
+  display: flex;
+  gap: 24px;
+`;
 
 export const MyPageProfileContainer = styled.div`
-	width: 30%;
-	height: 90%;
-	border: 1px solid #d9d9d9;
-	display: flex;
-	flex-direction: column;
-	gap: 20px;
-	box-shadow: 2px 0px 1px rgba(0, 0, 0, 0.1);
-`
+  width: 24%;
+  height: 90%;
+  border: 1px solid #d9d9d9;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  box-shadow: 2px 0px 5px 0px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+`;
 
 export const MyPageProfileTitleContainer = styled.div`
-	display: flex;
-	align-items: center;
-	padding: 16px 18px 16px 18px;
-`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 90%;
+  padding: 16px 18px 16px 18px;
+`;
 
 export const MyPageProfileTitleContainerTitle = styled.div`
-	font-size: 20px;
-	font-family: ${textStyle.fontFamily.P7};
-`
+  font-size: 20px;
+  font-family: ${textStyle.fontFamily.P6};
+`;
 
 export const MyPageProfileTitleContainerBadge = styled.div`
-	width: 66px;
-	height: 30px;
-	background: ${color.primary};
-	border: 1px solid #3772f7;
-	border-radius: 10px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	margin-left: auto;
-	font-size: 12px;
-	color: white;
-	font-family: ${textStyle.fontFamily.P6};
-`
+  width: 66px;
+  height: 30px;
+  background: ${color.secondary};
+  border: 1px solid #3772f7;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: auto;
+  font-size: 12px;
+  color: white;
+  font-family: ${textStyle.fontFamily.P6};
+`;
 
 export const MyPageProfileImageContainer = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
 export const MyPageProfileImage = styled(Image)`
-	width: 12vw;
-	height: 12vw;
-	border-radius: 999px;
-	background-color: ${color.line};
-	margin-bottom: 20px;
-`
+  width: 10vw;
+  height: 10vw;
+  border-radius: 999px;
+  background-color: ${color.line};
+  margin-bottom: 20px;
+`;
 
 export const MyPageProfileImageText = styled.span`
-	font-size: 16px;
-	font-family: ${textStyle.fontFamily.P5};
-	color: ${color.gray};
-	b {
-		color: black;
-		font-size: 22px;
-		font-family: ${textStyle.fontFamily.P7};
-	}
-`
+  font-size: 16px;
+  font-family: ${textStyle.fontFamily.P5};
+  color: ${color.gray};
+  b {
+    color: black;
+    font-size: 22px;
+    font-family: ${textStyle.fontFamily.P7};
+  }
+`;
+
+export const MyPageProfileList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 14px;
+  width: 70%;
+  padding: 14px 0 24px 0;
+  border-bottom: 1px solid #ccc;
+`;
+
+export const MyPageProfileListItem = styled.li`
+  list-style: none;
+  font-size: 16px;
+  font-family: ${textStyle.fontFamily.P7};
+  color: black;
+  margin-right: auto;
+`;
+
+export const MyPageInfomationBox = styled.aside`
+  width: 100%;
+
+  border: 1px solid ${color.on_tertiary};
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const MyPageInfomationHeader = styled.header`
+  width: 100%;
+  height: 40px;
+  font-size: 18px;
+  color: white;
+  font-family: ${textStyle.fontFamily.P7};
+  padding-left: 34px;
+  background-color: ${color.primary};
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  display: flex;
+  align-items: center;
+`;
+
+export const MyPageInfomationFigure = styled.figure<{ tree?: boolean; last?: boolean }>`
+  ${({ last }) =>
+    !last &&
+    css`
+      border-bottom: 1px solid ${color.tertiary_container};
+    `}
+  width: 96%;
+  height: 38px;
+  padding-left: ${({ tree }) => (tree ? '18px' : '52px')};
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  font-family: ${textStyle.fontFamily.P5};
+
+  button {
+    margin-left: auto;
+    width: 66px;
+    height: 25px;
+    cursor: pointer;
+    background-color: transparent;
+    border: 1px solid ${color.on_tertiary};
+    border-radius: 5px;
+  }
+`;
+
+export const MyPageInfomationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 76%;
+  gap: 4vh;
+`;
--- layouts/MyPageLayout.tsx
+++ layouts/MyPageLayout.tsx
@@ -1,28 +1,108 @@
-import React from 'react'
-import * as S from './MyPageLayout.style'
-import { users } from '@zipzoong/sdk/lib/functional'
-import { ICustomer } from '@zipzoong/sdk/lib/structures/user/customer'
+import React from 'react';
+import * as S from './MyPageLayout.style';
+import { ICustomer } from '@zipzoong/sdk/lib/structures/user/customer';
+import { IHSProvider } from '@zipzoong/sdk/lib/structures/user/hs_provider';
+import { IREAgent } from '@zipzoong/sdk/lib/structures/user/re_agent';
 
-const MyPageLayout = (data: ) => {
-	return (
-		<S.MyPageLayout>
-			<S.MyPageContainer>
-				<S.MyPageProfileContainer>
-					<S.MyPageProfileTitleContainer>
-						<S.MyPageProfileTitleContainerTitle>일반 회원</S.MyPageProfileTitleContainerTitle>
-						<S.MyPageProfileTitleContainerBadge>인증 완료</S.MyPageProfileTitleContainerBadge>
-					</S.MyPageProfileTitleContainer>
-					<S.MyPageProfileImageContainer>
-						<S.MyPageProfileImage src="" alt="" />
-						<S.MyPageProfileImageText>
-							<b>{data.name}</b>
-						</S.MyPageProfileImageText>
-						<S.MyPageProfileImageText>회원님</S.MyPageProfileImageText>
-					</S.MyPageProfileImageContainer>
-				</S.MyPageProfileContainer>
-			</S.MyPageContainer>
-		</S.MyPageLayout>
-	)
+interface MyPageLayoutPropsType {
+  data: ICustomer.IPrivate;
+  major: IHSProvider.IPrivate | IREAgent.IPrivate;
 }
 
-export default MyPageLayout
+const MyPageLayout = ({ data, major }: MyPageLayoutPropsType) => {
+  return (
+    <S.MyPageLayout>
+      <S.MyPageContainer>
+        <S.MyPageProfileContainer>
+          <S.MyPageProfileTitleContainer>
+            <S.MyPageProfileTitleContainerTitle>일반 회원</S.MyPageProfileTitleContainerTitle>
+            <S.MyPageProfileTitleContainerBadge>인증 완료</S.MyPageProfileTitleContainerBadge>
+          </S.MyPageProfileTitleContainer>
+          <S.MyPageProfileImageContainer>
+            <S.MyPageProfileImage src="/" alt="" width={100} height={100} />
+            <S.MyPageProfileImageText>
+              <b>{data.name}</b>
+            </S.MyPageProfileImageText>
+            <S.MyPageProfileImageText>
+              {!major.type && '회원님'}
+              {major.type === 'real estate agent' && '공인중개사님'}
+              {major.type === 'home service provider' && '생활전문가님'}
+            </S.MyPageProfileImageText>
+          </S.MyPageProfileImageContainer>
+          <S.MyPageProfileList>
+            <S.MyPageProfileListItem>나의 정보</S.MyPageProfileListItem>
+            <S.MyPageProfileListItem>내정보 관리</S.MyPageProfileListItem>
+            <S.MyPageProfileListItem>커뮤니티</S.MyPageProfileListItem>
+          </S.MyPageProfileList>
+          <S.MyPageProfileList>
+            <S.MyPageProfileListItem>공지 사항</S.MyPageProfileListItem>
+            <S.MyPageProfileListItem>고객 센터</S.MyPageProfileListItem>
+          </S.MyPageProfileList>
+        </S.MyPageProfileContainer>
+        <S.MyPageInfomationContainer>
+          <S.MyPageInfomationBox>
+            <S.MyPageInfomationHeader>내정보</S.MyPageInfomationHeader>
+            <S.MyPageInfomationFigure tree>
+              내정보
+              <button>수정</button>
+            </S.MyPageInfomationFigure>
+            <S.MyPageInfomationFigure>
+              본명 : {data.name}
+              <button>수정</button>
+            </S.MyPageInfomationFigure>
+            <S.MyPageInfomationFigure>
+              {data.phone}
+              <button>수정</button>
+            </S.MyPageInfomationFigure>
+            <S.MyPageInfomationFigure last>
+              {data.email}
+              <button>수정</button>
+            </S.MyPageInfomationFigure>
+          </S.MyPageInfomationBox>
+          {data.type !== 'customer' && (
+            <S.MyPageInfomationBox>
+              <S.MyPageInfomationHeader>포트폴리오 관리</S.MyPageInfomationHeader>
+              <S.MyPageInfomationFigure tree>
+                포트폴리오 관리 <button>확인</button>
+              </S.MyPageInfomationFigure>
+              {data.type === 'real estate agent' && (
+                <S.MyPageInfomationFigure tree>
+                  매물 수정/삭제
+                  <button>확인</button>
+                </S.MyPageInfomationFigure>
+              )}
+            </S.MyPageInfomationBox>
+          )}
+          {data.type === 'customer' && (
+            <S.MyPageInfomationBox>
+              <S.MyPageInfomationHeader>내정보 관리</S.MyPageInfomationHeader>
+              <S.MyPageInfomationFigure tree>
+                찜<button>관리</button>
+              </S.MyPageInfomationFigure>
+              <S.MyPageInfomationFigure tree>
+                집중 케어
+                <button>관리</button>
+              </S.MyPageInfomationFigure>
+              <S.MyPageInfomationFigure tree>
+                후기 관리
+                <button>관리</button>
+              </S.MyPageInfomationFigure>
+            </S.MyPageInfomationBox>
+          )}
+          <S.MyPageInfomationBox>
+            <S.MyPageInfomationHeader>커뮤니티</S.MyPageInfomationHeader>
+            <S.MyPageInfomationFigure tree>
+              내가 작성한 글 목록<button>확인</button>
+            </S.MyPageInfomationFigure>
+            <S.MyPageInfomationFigure tree>
+              내가 작성한 댓글 목록
+              <button>확인</button>
+            </S.MyPageInfomationFigure>
+          </S.MyPageInfomationBox>
+        </S.MyPageInfomationContainer>
+      </S.MyPageContainer>
+    </S.MyPageLayout>
+  );
+};
+
+export default MyPageLayout;
