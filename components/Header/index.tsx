288a40a931ab8e98e8d564b478ea968d5ae1836b
Ubinquitous <ubinquitous1@gmail.com>
2023-06-26 15:51:39 +0900

    UPDATE : header href, delete detail

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,52 +1,53 @@
-import React from 'react'
-import * as S from './style'
-import logo from 'assets/logo.svg'
-import color from '@/styles/color.theme'
-import search from 'assets/search.svg'
-import searchprimary from 'assets/searchprimary.svg'
-import { useRouter } from 'next/router'
+import React from 'react';
+import * as S from './style';
+import logo from 'assets/logo.svg';
+import color from '@/styles/color.theme';
+import search from 'assets/search.svg';
+import searchprimary from 'assets/searchprimary.svg';
+import { useRouter } from 'next/router';
+import service from '@/constants/service.constants';
 
 const Header = () => {
-	const router = useRouter()
+  const router = useRouter();
 
-	return (
-		<S.HeaderLayout>
-			<S.HeaderContainer>
-				<S.HeaderLogo src={logo} alt="" onClick={() => router.push('/')} />
-				<S.Navigation color={color.tertiary} href="/view/estate">
-					중개사매칭
-				</S.Navigation>
-				<S.Navigation color={color.tertiary} href="/view/life">
-					생활서비스
-				</S.Navigation>
-				<S.Navigation color={color.tertiary} href="/">
-					집중케어
-				</S.Navigation>
-				<S.Navigation color={color.tertiary} href="/">
-					커뮤니티
-				</S.Navigation>
-				<S.SearchContainer>
-					<S.SearchInput placeholder="검색어를 입력하세요" />
-					<S.SearchButton>
-						<S.SearchLogo src={search} alt="" />
-					</S.SearchButton>
-				</S.SearchContainer>
-				<S.Navigation ismediaquery="true" color={color.gray} href="/">
-					전문가등록
-				</S.Navigation>
-				<S.Navigation color={color.gray} href="/login">
-					로그인
-				</S.Navigation>
-				<S.SignupButton href="/signup">회원가입</S.SignupButton>
-			</S.HeaderContainer>
-			<S.MediaQuerySearchContainer>
-				<S.SearchInput placeholder="검색어를 입력하세요" />
-				<S.SearchButton>
-					<S.SearchLogo src={searchprimary} alt="" />
-				</S.SearchButton>
-			</S.MediaQuerySearchContainer>
-		</S.HeaderLayout>
-	)
-}
+  return (
+    <S.HeaderLayout>
+      <S.HeaderContainer>
+        <S.HeaderLogo src={logo} alt="" onClick={() => router.push('/')} />
+        <S.Navigation color={color.tertiary} href={`/view?type=${service.RE_AGENTS}`}>
+          중개사매칭
+        </S.Navigation>
+        <S.Navigation color={color.tertiary} href={`/view?type=${service.HS_PROVIDER}`}>
+          생활서비스
+        </S.Navigation>
+        <S.Navigation color={color.tertiary} href="/">
+          집중케어
+        </S.Navigation>
+        <S.Navigation color={color.tertiary} href="/">
+          커뮤니티
+        </S.Navigation>
+        <S.SearchContainer>
+          <S.SearchInput placeholder="검색어를 입력하세요" />
+          <S.SearchButton>
+            <S.SearchLogo src={search} alt="" />
+          </S.SearchButton>
+        </S.SearchContainer>
+        <S.Navigation ismediaquery="true" color={color.gray} href="/">
+          전문가등록
+        </S.Navigation>
+        <S.Navigation color={color.gray} href="/login">
+          로그인
+        </S.Navigation>
+        <S.SignupButton href="/signup">회원가입</S.SignupButton>
+      </S.HeaderContainer>
+      <S.MediaQuerySearchContainer>
+        <S.SearchInput placeholder="검색어를 입력하세요" />
+        <S.SearchButton>
+          <S.SearchLogo src={searchprimary} alt="" />
+        </S.SearchButton>
+      </S.MediaQuerySearchContainer>
+    </S.HeaderLayout>
+  );
+};
 
-export default Header
+export default Header;
